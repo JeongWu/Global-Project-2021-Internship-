305b3a7040001166d7a53562376334aeb58eb96e
jane <park96100331@gmail.com>
2021-03-05 14:34:10 +0900

    update test view and pass 'code'

--- okr/src/main/java/com/eximbay/okr/api/DictionaryAPI.java
+++ okr/src/main/java/com/eximbay/okr/api/DictionaryAPI.java
@@ -1,9 +1,15 @@
 package com.eximbay.okr.api;
 
-import com.eximbay.okr.constant.DictionaryType;
+
 import com.eximbay.okr.dto.dictionary.DictionaryDto;
+import com.eximbay.okr.entity.CodeList;
+
 import com.eximbay.okr.entity.Dictionary;
+import com.eximbay.okr.model.dictionary.DictionaryViewModel;
+import com.eximbay.okr.model.dictionary.SelectTypeModel;
+import com.eximbay.okr.repository.CodeListRepository;
 import com.eximbay.okr.repository.DictionaryRepository;
+import com.eximbay.okr.service.Interface.IDictionaryService;
 import com.eximbay.okr.utils.MapperUtil;
 
 import lombok.AllArgsConstructor;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/dictionary")
@@ -21,7 +28,8 @@ import java.util.List;
 public class DictionaryAPI {
     
     private final DictionaryRepository dictionaryRepository;
-
+    private final CodeListRepository codeListRepository;
+    private final IDictionaryService dictionaryService;
     private final MapperFacade mapper;
 
     @ResponseBody
@@ -34,11 +42,26 @@ public class DictionaryAPI {
 
     @ResponseBody
     @RequestMapping("/keyResult/datatables")
-    public List<DictionaryDto> getDictionaryOfKeyResult() {
-        List<Dictionary> dictionaries = dictionaryRepository.findByDictionaryType(DictionaryType.KEY_RESULT);
-        // List<DictionaryDto> dictionaryList = MapperUtil.mapList(dictionaries, DictionaryDto.class);
-        // return dictionaryList;
-        return mapper.mapAsList(dictionaries, DictionaryDto.class);
+    public List<DictionaryViewModel> getDictionaryOfKeyResult() {
+        List<Dictionary> dictionaries = dictionaryRepository.findByDictionaryType("KEY_RESULT");
+        List<DictionaryViewModel> viewModels=mapper.mapAsList(dictionaries, DictionaryViewModel.class);
+        SelectTypeModel selectTypeModel = dictionaryService.buildSelectTypeModel();
+        for(int i=0;i<viewModels.size();i++){
+            String code1=viewModels.get(i).getJobType();
+            Optional<CodeList> jobType=selectTypeModel.getJobType().stream().filter(d->d.getCodeListId().getCode().equals(code1)).findFirst();
+            viewModels.get(i).setJobTypeCodeName(jobType.get().getCodeName());
+            String code2=viewModels.get(i).getTaskType();
+            Optional<CodeList> taskType=selectTypeModel.getTaskType().stream().filter(d->d.getCodeListId().getCode().equals(code2)).findFirst();
+            viewModels.get(i).setTaskTypeCodeName(taskType.get().getCodeName());
+            String code3=viewModels.get(i).getTaskMetric();
+            Optional<CodeList> taskMetric=selectTypeModel.getTaskMetric().stream().filter(d->d.getCodeListId().getCode().equals(code3)).findFirst();
+            viewModels.get(i).setTaskMetricCodeName(taskMetric.get().getCodeName());
+            String code4=viewModels.get(i).getTaskIndicator();
+            Optional<CodeList> taskIndicator=selectTypeModel.getTaskIndicator().stream().filter(d->d.getCodeListId().getCode().equals(code4)).findFirst();
+            viewModels.get(i).setTaskIndicatorCodeName(taskIndicator.get().getCodeName());
+        }
+
+        return viewModels;
     }
 }
 
