697f5bac143e845368562c31efcd69c16edb7939
jane <park96100331@gmail.com>
2021-03-10 15:26:54 +0900

    fix errors with dictionary list

--- okr/src/main/java/com/eximbay/okr/api/DictionaryAPI.java
+++ okr/src/main/java/com/eximbay/okr/api/DictionaryAPI.java
@@ -1,7 +1,6 @@
 package com.eximbay.okr.api;
 
 import com.eximbay.okr.constant.DictionaryType;
-import com.eximbay.okr.dto.dictionary.DictionaryDto;
 import com.eximbay.okr.entity.CodeList;
 
 import com.eximbay.okr.entity.Dictionary;
@@ -10,15 +9,13 @@ import com.eximbay.okr.model.dictionary.SelectTypeModel;
 import com.eximbay.okr.repository.DictionaryRepository;
 import com.eximbay.okr.service.Interface.IDictionaryService;
 import com.eximbay.okr.service.specification.DictionaryQuery;
-import com.eximbay.okr.utils.MapperUtil;
 
 import lombok.AllArgsConstructor;
 import ma.glasnost.orika.MapperFacade;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,29 +30,60 @@ public class DictionaryAPI {
     private final MapperFacade mapper;
     private final DictionaryQuery dictionaryQuery;
 
-
     @PostMapping("/datatables")
-    public List<DictionaryDto> getDictionaryData() {
-         List<DictionaryDto> dictionaries = dictionaryService.findAll();
-        return dictionaries;
+    public List<DictionaryViewModel> getDictionaryData() {
+        List<Dictionary> dictionaries = dictionaryRepository.findAll();
+
+        List<DictionaryViewModel> viewModels = mapper.mapAsList(dictionaries, DictionaryViewModel.class);
+        SelectTypeModel selectTypeModel = dictionaryService.buildSelectTypeModel();
+
+        for (DictionaryViewModel model : viewModels) {
+            Optional<CodeList> dictionaryType = selectTypeModel.getDictionaryType().stream()
+                    .filter(d -> d.getCodeListId().getCode().equals(model.getDictionaryType())).findFirst();
+            model.setDictionaryTypeCodeName(dictionaryType.get().getCodeName());
+
+            Optional<CodeList> jobType = selectTypeModel.getJobType().stream()
+                    .filter(d -> d.getCodeListId().getCode().equals(model.getJobType())).findFirst();
+            model.setJobTypeCodeName(jobType.get().getCodeName());
+
+            if(model.getDictionaryType().equals(DictionaryType.COMPETENCE)){
+            Optional<CodeList> categoryGroup = selectTypeModel.getCategoryGroup().stream()
+                    .filter(d -> d.getCodeListId().getCode().equals(model.getCategoryGroup())).findFirst();
+            model.setCategoryGroupCodeName(categoryGroup.get().getCodeName());
+
+            Optional<CodeList> category = selectTypeModel.getCategory().stream()
+                    .filter(d -> d.getCodeListId().getCode().equals(model.getCategory())).findFirst();
+            model.setCategoryCodeName(category.get().getCodeName());
+
+            }
+            
+
+        }
+
+        return viewModels;
     }
 
     @RequestMapping("/keyResult/datatables")
     public List<DictionaryViewModel> getDictionaryOfKeyResult() {
-       
-        List<Dictionary> dictionaries =dictionaryRepository.findAll(dictionaryQuery.findByDictionaryType(DictionaryType.KEY_RESULT).and(dictionaryQuery.findActiveDictionaryOnly()));
 
-        List<DictionaryViewModel> viewModels=mapper.mapAsList(dictionaries, DictionaryViewModel.class);
+        List<Dictionary> dictionaries = dictionaryRepository.findAll(dictionaryQuery
+                .findByDictionaryType(DictionaryType.KEY_RESULT).and(dictionaryQuery.findActiveDictionaryOnly()));
+
+        List<DictionaryViewModel> viewModels = mapper.mapAsList(dictionaries, DictionaryViewModel.class);
         SelectTypeModel selectTypeModel = dictionaryService.buildSelectTypeModel();
-        
+
         for (DictionaryViewModel model : viewModels) {
-            Optional<CodeList> availableJobType = selectTypeModel.getJobType().stream().filter(d->d.getCodeListId().getCode().equals(model.getJobType())).findFirst();
+            Optional<CodeList> availableJobType = selectTypeModel.getJobType().stream()
+                    .filter(d -> d.getCodeListId().getCode().equals(model.getJobType())).findFirst();
             model.setJobTypeCodeName(availableJobType.get().getCodeName());
-            Optional<CodeList> availableTaskType = selectTypeModel.getTaskType().stream().filter(d->d.getCodeListId().getCode().equals(model.getTaskType())).findFirst();
+            Optional<CodeList> availableTaskType = selectTypeModel.getTaskType().stream()
+                    .filter(d -> d.getCodeListId().getCode().equals(model.getTaskType())).findFirst();
             model.setTaskTypeCodeName(availableTaskType.get().getCodeName());
-            Optional<CodeList> availableTaskMetric = selectTypeModel.getTaskMetric().stream().filter(d->d.getCodeListId().getCode().equals(model.getTaskMetric())).findFirst();
+            Optional<CodeList> availableTaskMetric = selectTypeModel.getTaskMetric().stream()
+                    .filter(d -> d.getCodeListId().getCode().equals(model.getTaskMetric())).findFirst();
             model.setTaskMetricCodeName(availableTaskMetric.get().getCodeName());
-            Optional<CodeList> availableTaskIndicator = selectTypeModel.getTaskIndicator().stream().filter(d->d.getCodeListId().getCode().equals(model.getTaskIndicator())).findFirst();
+            Optional<CodeList> availableTaskIndicator = selectTypeModel.getTaskIndicator().stream()
+                    .filter(d -> d.getCodeListId().getCode().equals(model.getTaskIndicator())).findFirst();
             model.setTaskIndicatorCodeName(availableTaskIndicator.get().getCodeName());
         }
 
