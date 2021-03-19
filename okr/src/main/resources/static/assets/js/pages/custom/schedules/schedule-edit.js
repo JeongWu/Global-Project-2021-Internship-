0b26acf6c3712be1f78b39cd6cfc021d760b2098
jane <park96100331@gmail.com>
2021-03-19 18:28:32 +0900

    edit schedule update (validation&confirm)

--- okr/src/main/resources/static/assets/js/pages/custom/schedules/schedule-edit.js
+++ okr/src/main/resources/static/assets/js/pages/custom/schedules/schedule-edit.js
@@ -1,22 +1,19 @@
-const validate_quarterly_Day = function (e) {
-//   var StartDate = document.getElementById("quarterly_beginDay").value;
+const validate_Day = function () {
+  console.log($(this))
   var StartDate = $("#quarterly_beginDay").val();
   var EndDate = $("#quarterly_endDay").val();
-  console.log(e);
-//   var EndDate = document.getElementById("quarterly_endDay").value;
-  //var eDate = new Date(EndDate);
-  //var sDate = new Date(StartDate);
+
   var sDate = parseInt(StartDate);
   var eDate = parseInt(EndDate);
   if (sDate > eDate) {
     alert("The begin day must be ealier than the end day");
-    // return false;
+    $(this).val("");
   }
-//   return true;
 };
+
 const validate_monthly_Day = function () {
-  var StartDate = document.getElementById("monthly_beginDay").value;
-  var EndDate = document.getElementById("monthly_endDay").value;
+  var StartDate = $("#monthly_beginDay").val();
+  var EndDate = $("#monthly_endDay").val();
   var sDate = parseInt(StartDate);
   var eDate = parseInt(EndDate);
   if (StartDate != "" && StartDate != "" && sDate > eDate) {
@@ -36,13 +33,18 @@ jQuery(document).ready(function () {
   $("#weekly_select").val($("#weekly_input").val().split(","));
   $("#quarterly_select,#monthly_select,#weekly_select").select2({
     placeholder: "Select..",
+    maximumSelectionLength: 3
   });
- 
 
-  $("#quarterly_beginDay").on("change", validate_quarterly_Day);
-  $("#quarterly_endDay").on("change", validate_quarterly_Day);
-  $("#monthly_beginDay").on("change", validate_quarterly_Day);
-  $("#monthly_endDay").on("change", validate_quarterly_Day);
+  $("#quarterly_beginDay").on("change", validate_Day);
+  $("#quarterly_endDay").on("change", validate_Day);
+  $("#monthly_beginDay").on("change", validate_Day);
+  $("#monthly_endDay").on("change", validate_Day);
+
+  $("#quarterly_select,#monthly_select,#weekly_select").on("select2:unselecting", function(){
+    //confirm 추가하기
+    alert('Are you sure?');
+  });
 
 
 });
