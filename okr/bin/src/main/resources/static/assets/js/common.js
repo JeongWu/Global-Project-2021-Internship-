625f11f020fc8cd56610d7aee0f0759f41cca4e3
jane <park96100331@gmail.com>
2021-03-26 09:10:07 +0900

    clean bin

--- okr/bin/src/main/resources/static/assets/js/common.js
+++
@@ -1,7 +0,0 @@
-function buildPageSizeSelection(){
-    let baseUrl = document.getElementById("base-url").value;
-    let pageSize = document.getElementById("page-size-selection");
-    pageSize.addEventListener("change", ()=>{
-        window.location.href = baseUrl+"?size="+pageSize.value
-    })
-}
